fc226c8c72b0a58a30239f9e365616f4f743318a
AhmedAshraf259 <ahmedashraf88513@gmail.com>
2026-09-19 01:59:44 +0300

    Fix DATABASE_URL handling for Vercel

--- app/database.py
+++ app/database.py
@@ -3,15 +3,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
+# جلب رابط قاعدة البيانات أو استخدام SQLite مؤقتاً في مسار tmp الخاطف الخاص بـ Vercel
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# التعامل مع مسارات SQLite في Serverless
-if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-    )
-else:
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+if not DATABASE_URL:
+    # بيئة Vercel تقبل الكتابة فقط داخل المجلد المؤقت /tmp
+    DATABASE_URL = "sqlite:////tmp/sql_app.db"
+
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
